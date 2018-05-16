10442ebbb5a69263c5acda038722cdddcfb9623d
Dhruv Rubyist <dhruv@joshsoftware.com>
2018-05-16 15:36:52 +0530

    WIP

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+const moduleObj = {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'  
+        }
+    ]
+}
+
+const client = {
+    entry: {
+        'client': './src/client/index.js'
+    },
+    target: 'web',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist/public')
+    },
+    module: moduleObj,
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/client/index.html'
+        })
+    ]
+};
+
+const server = {
+    entry: {
+        'server': './src/server/index.js'
+    },
+    target: 'node',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist')
+    },
+    externals: [nodeExternals()],
+    module: moduleObj
+}
+
+module.exports = [client, server];
+
